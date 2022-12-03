aa1957be873258351cebc15148b7b50ea50397d5
1vanK <1vanK@users.noreply.github.com>
2022-12-04 02:59:54 +0300

    Спрессованные коммиты

---
+++ engine/2_cmake_vs.bat
@@ -0,0 +1,13 @@
+:: Меняем кодировку консоли на UTF-8
+chcp 65001
+
+:: Указываем путь к cmake.exe
+set "PATH=c:\programs\cmake\bin"
+
+:: Создаём проекты для Visual Studio 2019 в папке build_vs, используя конфиг CMakeLists.txt из папки repo
+:: https://urho3d-doxygen.github.io/doxygen/_building.html
+cmake.exe repo -B build_vs -G "Visual Studio 16" -A x64 -D URHO3D_OPENGL=1 -D URHO3D_D3D11=1^
+ -D URHO3D_SAMPLES=0 -D URHO3D_TOOLS=0
+
+:: Ждём нажатие Enter перед закрытием консоли
+pause
---
+++ engine/3_build_vs.bat
@@ -0,0 +1,11 @@
+:: Меняем кодировку консоли на UTF-8
+chcp 65001
+
+:: Указываем путь к cmake.exe
+set "PATH=c:\programs\cmake\bin"
+
+:: Компилируем проекты в папке build_vs.
+cmake --build build_vs --config Release
+
+:: Ждём нажатие Enter перед закрытием консоли
+pause
