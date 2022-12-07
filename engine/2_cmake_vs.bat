676278a9b260dedcc0bd131e24698a0b444174c4
1vanK <1vanK@users.noreply.github.com>
2022-12-07 09:48:31 +0300

    Update 2_cmake_vs.bat

--- engine/2_cmake_vs.bat
+++ engine/2_cmake_vs.bat
@@ -7,7 +7,7 @@ set "PATH=c:\programs\cmake\bin"
 :: Создаём проекты для Visual Studio 2019 в папке build_vs, используя конфиг CMakeLists.txt из папки repo
 :: https://urho3d-doxygen.github.io/doxygen/_building.html
 cmake.exe repo -B build_vs -G "Visual Studio 16" -A x64 -D URHO3D_OPENGL=1 -D URHO3D_D3D11=1^
- -D URHO3D_SAMPLES=0 -D URHO3D_TOOLS=0
+ -D URHO3D_SAMPLES=0 -D URHO3D_TOOLS=1
 
 :: Ждём нажатие Enter перед закрытием консоли
 pause
