d453abc036b905975623cb148193a643df15282d
1vanK <1vanK@users.noreply.github.com>
2022-12-07 09:34:56 +0300

    Update 3_build_vs.bat

--- engine/3_build_vs.bat
+++ engine/3_build_vs.bat
@@ -4,7 +4,7 @@ chcp 65001
 :: Указываем путь к cmake.exe
 set "PATH=c:\programs\cmake\bin"
 
-:: Компилируем проекты в папке build_vs.
+:: Компилируем проекты в папке build_vs
 cmake --build build_vs --config Release
 
 :: Ждём нажатие Enter перед закрытием консоли
